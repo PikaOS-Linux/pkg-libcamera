4b1ff5c56a157d2c0e5b09d4f27e0cf6012212d0
ferrreo <harderthanfire@gmail.com>
2023-06-16 18:52:45 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,8 +1,3 @@
-# Add dependent repositories
-wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
-add-apt-repository https://ppa.pika-os.com
-add-apt-repository ppa:pikaos/pika
-add-apt-repository ppa:kubuntu-ppa/backports
 # Clone Upstream
 git clone https://git.libcamera.org/libcamera/libcamera.git/ -b v0.0.5
 cp -rvf ./debian ./libcamera/
@@ -13,8 +8,6 @@ for i in ../patches/*.patch; do patch -Np1 -i $i ;done
 LOGNAME=root dh_make --createorig -y -l -p libcamera_0.0.5
 
 # Get build deps
-ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
-DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
 apt-get build-dep ./ -y
 
 # Build package
