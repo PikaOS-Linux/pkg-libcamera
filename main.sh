23454ac825b20788f1d592036b6bec85556ec79a
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-09-30 13:07:37 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,7 +5,7 @@ git clone https://git.libcamera.org/libcamera/libcamera.git/ -b v0.1.0
 cp -rvf ./debian ./libcamera/
 cd ./libcamera
 
-for i in "$(cat ../patches/series)" ; do echo "Applying Patch: $i" && patch -Np1 -i ../$i || echo "Applying Patch $i Failed!"; done
+for i in "$(cat ../patches/series)" ; do echo "Applying Patch: $i" && patch -Np1 -i ../patches/$i || echo "Applying Patch $i Failed!"; done
 
 LOGNAME=root dh_make --createorig -y -l -p libcamera_0.1.0
 
