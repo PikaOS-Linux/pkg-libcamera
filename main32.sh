8dc12c4603677023f00a9b33ed02b43980600687
Ward from fusion-voyager-3 <hotrod.master@hotmail.com>
2024-02-26 19:03:33 +0300

    update to 0.2.0

--- main32.sh
+++ main32.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
 # Clone Upstream
-git clone https://git.libcamera.org/libcamera/libcamera.git/ -b v0.1.0
+git clone https://git.libcamera.org/libcamera/libcamera.git/ -b v0.2.0
 cp -rvf ./debian ./libcamera/
 cd ./libcamera
 
 for i in $(cat ../patches/series) ; do echo "Applying Patch: $i" && patch -Np1 -i ../patches/$i || echo "Applying Patch $i Failed!"; done
 
-LOGNAME=root dh_make --createorig -y -l -p libcamera_0.1.0
+LOGNAME=root dh_make --createorig -y -l -p libcamera_0.2.0
 
 # Get build deps
 apt-get build-dep ./ -y
